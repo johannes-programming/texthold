3dd7ab4f14ddb8cca006e29da87adc3f11441250
Johannes <johannes-programming@mailfence.com>
2025-01-22 19:45:18 +0100

    Version 0.0.1

--- src/texthold/__init__.py
+++ src/texthold/__init__.py
@@ -1,5 +1,2 @@
 from texthold.core import *
 from texthold.tests import *
-
-if __name__ == "__main__":
-    main()
--- src/texthold/__main__.py
+++
@@ -1,4 +0,0 @@
-from texthold import main
-
-if __name__ == "__main__":
-    main()
